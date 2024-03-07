bd7b4e13ae2838f3149495d6c78b585ba64abd19
toastsandwich <shreyasxcore2003@gmail.com>
2024-03-07 23:19:31 +0530

    refined the server

--- v1.0.0/server/main.go
+++ v1.0.0/server/main.go
@@ -1,81 +1,88 @@
 package main
 
 import (
-	"bufio"
-	"log"
+	"fmt"
 	"net"
 )
 
 const (
-	addr = "localhost:7568"
+	addr = "192.168.1.106:8647"
 	tcp  = "tcp"
 )
 
 type server struct {
-	addr      string
-	clients   map[net.Conn]struct{}
-	addClient chan net.Conn
-	rmClient  chan net.Conn
-	message   chan string
+	clients    map[net.Conn]bool
+	addClients chan net.Conn
+	rmClients  chan net.Conn
+	messages   chan string
 }
 
-func newServer(addr string) *server {
+func newServer() *server {
 	return &server{
-		addr:      addr,
-		clients:   make(map[net.Conn]struct{}),
-		addClient: make(chan net.Conn),
-		rmClient:  make(chan net.Conn),
-		message:   make(chan string),
+		clients:    make(map[net.Conn]bool),
+		addClients: make(chan net.Conn),
+		rmClients:  make(chan net.Conn),
+		messages:   make(chan string),
 	}
 }
 
-func (s *server) start() {
-	ln, err := net.Listen(tcp, s.addr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	go s.handleMessages()
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
-		go s.handleConn(conn)
-	}
-}
-
-func (s *server) handleConn(conn net.Conn) {
+func (s *server) handleConnection(conn net.Conn) {
 	defer func() {
-		s.rmClient <- conn
+		s.rmClients <- conn
 		conn.Close()
 	}()
-	reader := bufio.NewReader(conn)
+	buf := make([]byte, 2048)
 	for {
-		message, err := reader.ReadString('\n')
-		if err != nil {
-			break
-		}
-		s.message <- message
+		n, _ := conn.Read(buf)
+		mssg := string(buf[:n])
+		s.messages <- mssg
 	}
 }
 
 func (s *server) handleMessages() {
 	for {
 		select {
-		case conn := <-s.addClient:
-			s.clients[conn] = struct{}{}
-		case conn := <-s.rmClient:
+		case conn := <-s.rmClients:
 			delete(s.clients, conn)
-		case mssg := <-s.message:
-			for conn, _ := range s.clients {
-				writer := bufio.NewWriter(conn)
-				writer.WriteString(mssg)
+		case conn := <-s.addClients:
+			s.clients[conn] = true
+		case mssg := <-s.messages:
+			for conn := range s.clients {
+				_, err := conn.Write([]byte(fmt.Sprintf("%s :: %s\n", conn.RemoteAddr().String(), mssg)))
+				if err != nil {
+					s.rmClients <- conn
+					conn.Close()
+				}
 			}
 		}
 	}
 }
 
+func (s *server) start() error {
+	ln, err := net.Listen(tcp, addr)
+	defer func() {
+		err := ln.Close()
+		if err != nil {
+			return
+		}
+	}()
+	if err != nil {
+		return err
+	}
+	go s.handleMessages()
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			continue
+		}
+		s.addClients <- conn
+		go s.handleConnection(conn)
+	}
+}
+
 func main() {
-	app := newServer(addr)
+	app := newServer()
+	fmt.Println("server hosted on ", addr)
+	// log.Fatal(app.start())
 	app.start()
 }
